94a33204d591ec165e3aea84b3e3bcdda8d67c3c
hugo <hugoacaz@umich.edu>
2024-12-14 22:36:50 -0500

    initialized API me thinks

--- youtube_api.py
+++ youtube_api.py
@@ -1,3 +1,7 @@
 from googleapiclient.discovery import build
 
 # use pip install google-api-python-client in terminal to get API
+
+api_key = "AIzaSyA51rPuo9cvxTta8y1G0zPf3sjct0-lah8"
+youtube = build('youtube', 'v3', developerKey=api_key)
+
