81c95a5812113b3055712421134853ce91fe950a
hugo <hugoacaz@umich.edu>
2024-12-14 22:00:52 -0500

    edited the code so the client id's are not hard coded

--- spotify_api.py
+++ spotify_api.py
@@ -4,10 +4,9 @@ from spotipy.oauth2 import SpotifyOAuth
 # Make sure to type "pip install spotipy --upgrade" in terminal to use spotipy
 # here is a guide for spotipy: https://spotipy.readthedocs.io/en/2.24.0/#getting-started
 
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id="96c0eaa3eb774eb4a1ddbcc8db88e3fc",
-                                               client_secret="907e866598ad4cfcb9038fdcae86f4cd",
+id = "96c0eaa3eb774eb4a1ddbcc8db88e3fc"
+secret_id = "907e866598ad4cfcb9038fdcae86f4cd"
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id= id,
+                                               client_secret= secret_id,
                                                redirect_uri="http://localhost:8888/callback",
                                                scope="user-library-read"))
-
-
-
