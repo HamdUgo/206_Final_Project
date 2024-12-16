5cd9f7f9368c9f1dd072bbf8e31e0abf4a6b852f
hugo <hugoacaz@umich.edu>
2024-12-16 11:23:17 -0500

    edited the code, api did not work gonna try to find alternative later

--- tiktok_api.py
+++ tiktok_api.py
@@ -1,9 +1,20 @@
-# use "pip install TikTokApi
-# python -m playwright install" in the terminal to get unofficial tiktok API
-from TikTokApi import TikTokApi
-try:
-    api = TikTokApi.get_instance()
-except:
-    print('error occured')
+import json
 
+    
+#saves data from API to a JSON file
+def save_json(data, file):
+    with open(file, 'w') as f:
+        json.dump(data, f, indent=4)
 
+#updates JSON file with new data
+def update_json(new_data, file):
+    try:
+        with open(file, 'r') as f:
+            data = json.load(f)
+    except:
+        data = []
+    
+    data.extend(new_data)
+
+    with open(file, 'w') as f:
+        json.dump(data, f, indent=4)
